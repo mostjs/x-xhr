7fe0dcdd34742019b947fd20d86e4075bed8d8ab
Brian Cavalier <brian@briancavalier.com>
2017-10-23 09:14:12 -0400

    chore(xhrStream): remove outdated comment

--- src/xhrStream.js
+++ src/xhrStream.js
@@ -6,19 +6,6 @@ import { XHRDisposable } from './XHRDisposable'
 
 /* global XMLHttpRequest, ProgressEvent */
 
-// Minimalist XHR stream
-// Provide a function to setup the XHR however
-// you want/need, but don't call send().  The returned
-// stream will call send(), handle XHR events, and
-// call abort() if necessary.
-// The returned Stream will contain at most 1 events:
-// the ProgressEvent emitted by XHR's 'load' OR 'error'
-// event.  You can detect errors and turn them into
-// Stream failures using chain() if you need.
-// chain(progressEvent =>
-//   progressEvent.type === 'error'
-//     ? throwError(new Error(...))
-//     : just(event), xhrStream(...))
 export const xhrStream = (createXHR: () => XMLHttpRequest): Stream<ProgressEvent> =>
   new XHRStream(createXHR)
 
